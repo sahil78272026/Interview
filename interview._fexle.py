4fe0e37c2a2ac5de9b9965a973b22680aebc1527
Sahil Garg <sahil.garg78272026@gmail.com>
2023-04-26 08:48:36 +0530

    Update interview._fexle.py

--- interview._fexle.py
+++ interview._fexle.py
@@ -1,16 +1,17 @@
-# Findind Leader in an array
 
-def printLeaders(arr,size):
-     
-    for i in range(0, size):
+def printLeaders(arr,size): 
+      
+    for i in range(0, size): 
         for j in range(i+1, size):
-            if arr[i]<=arr[j]:
-                
+            print('j in inner loop: ',j) 
+            if arr[i]<=arr[j]: 
+                print(f'break arr[i]:{arr[i]}, arr[j]:{arr[j]} ')
                 break
-        if j == size-1: # If loop didn't break
-            
-            print (arr[i],end=' ')
- 
-# Driver function
-arr=[16, 17, 4, 3, 5, 2]
-printLeaders(arr, len(arr))
+        print('j after inner loop:', j)
+        if j == size-1: # If loop didn't break 
+            print ('leader: ',arr[i],end=' ') 
+  
+# Driver function 
+arr=[16, 17, 4, 3, 4, 5, 4, 2] 
+printLeaders(arr, len(arr)) 
+  
