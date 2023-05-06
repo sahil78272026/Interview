ccd582173a6fcc576df0908001a2f97d6e8ba7ea
Sahil Garg <sahil.garg78272026@gmail.com>
2023-05-06 17:40:13 +0530

    Add files via upload

---
+++ interview_expand_my_business.py
@@ -0,0 +1,73 @@
+
+
+#map method
+
+
+# [1,4,9,16,25,36]
+
+"""def sq():
+    for i in lst:
+        print(i*i)
+
+
+lst = [1,2,3,4,5,6]
+map(sq,lst)"""
+
+
+
+"""strg = 'aeeabccbde'
+
+dict = {}
+
+for i in strg:
+    if i not in dict:
+        dict[i]=1
+    else:
+        dict[i]+=1
+
+print(dict)
+
+newDict ={}
+for i in dict:
+    if dict[i]%2==0:
+        newDict[i] = 1
+
+
+
+
+User
+    atmcardno
+    pin:
+
+Account
+    savings
+    current
+
+
+Atm
+    atmmachineno
+
+Money
+    notes
+    100
+    2000
+    500
+
+select sal from emp orderby sal desc 
+"""
+
+# find the nth prime number 5
+# senior python dev
+# erp, saas
+# hrms, like urban
+# snapcom, 
+
+"""
+database
+django
+django rest framework
+
+    
+
+
+
