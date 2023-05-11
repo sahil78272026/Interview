4da0f70e03e6495fbc8561274c940ea2bfeaf130
sahil.garg78272026@gmail.com <sahil.garg78272026@gmail.com>
2023-05-11 15:53:26 +0530

    reliance_jio

---
+++ reliance_jio.py
@@ -0,0 +1,16 @@
+
+# different databases you have used?
+# what are web servers?
+# whats is django orm
+# have you used bootstrap?
+# have you used 
+# caching, sessions,
+# django signals?
+# can use different database in one django application?
+# different types of datastructures avaialble in python
+# django architecture
+# BGP in network
+# difference between classbased view and function based view?
+# what are different modules/packages you have used?
+# what are django exceptions?
+
