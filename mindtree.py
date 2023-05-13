bc83ba9069aee0cc34c52aab86fcaea1b191ae19
sahil.garg78272026@gmail.com <sahil.garg78272026@gmail.com>
2023-05-13 11:32:33 +0530

    mindtree

---
+++ mindtree.py
@@ -0,0 +1,47 @@
+# date : 13-May-2023
+
+"""class myname:
+    ltimindtree,france,ltimindtree= range(3)
+
+
+print(myname.ltimindtree)   
+print(myname.france)
+print(myname.ltimindtree)
+
+
+"""
+
+
+"""a="ltimindtree"
+a[5]="d"
+print(a)
+"""
+
+"""z = [1,4,2,1,6,2,1]
+
+newDict ={}
+for i in z:
+    if i not in newDict:
+        newDict[i] = 1
+    else:
+        newDict[i] += 1
+
+print(newDict)
+"""
+#{1: 3, 2: 2, 4: 1, 6: 1}
+
+
+tup1 = (1,)
+
+
+
+# select * from Employee ordrby salary desc 
+
+# architect, restapi, django, docker, 
+# third hishest salary from employee
+# iterators, generators
+# how many types of inheritence, heirarchy
+# databases
+# do you know about docker
+# composite key
+# default django models used in your project
