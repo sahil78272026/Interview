87bbfc9e97a2ea4e334bcf6b1de670b2953e79f5
sahil.garg78272026@gmail.com <sahil.garg78272026@gmail.com>
2023-05-15 18:29:58 +0530

    virtualint

---
+++ virtualinterview.py
@@ -0,0 +1,7 @@
+# have you deployed any project?
+# Is any of your project Live?
+# have you used class based views?
+# what is APIView view?
+# can we send data directly from models to front end, without converting
+# difference between static root/ static
+# can we remove os.path.join?
