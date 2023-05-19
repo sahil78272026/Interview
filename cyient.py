1ea9869b9207f277498ba25b7f52c11c7371f82d
sahil.garg78272026@gmail.com <sahil.garg78272026@gmail.com>
2023-05-19 14:28:18 +0530

    cyient

---
+++ cyient.py
@@ -0,0 +1,14 @@
+for i in range(100):
+    if i%3==0 and i%5==0:
+        print("Fizbuzz")
+    elif i%3 == 0:
+        print("fiz")
+    elif i%5==0:
+        print("Buzz")
+
+
+# mixins
+# when does queryset API hits the database
+# select related and pre fetch related
+# how would you create a RestAPI
+# what are routers in DRF
