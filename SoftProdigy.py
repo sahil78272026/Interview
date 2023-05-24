a0c576de61c3a7d8eade4b781d1911b4249e943f
Sahil Garg <sahil.garg78272026@gmail.com>
2023-05-24 16:01:56 +0530

    Create SoftProdigy.py

---
+++ SoftProdigy.py
@@ -0,0 +1,60 @@
+"""even = lambda a: [num for num in range(a) if num%2 ==0]
+"""
+
+"""
+Write a program to Display the count of each word  from the given  sentence.  "word1 word2 word3 word1 word3 word4 word1 word5 word6"
+"""
+
+"""str1 = "word1 word2 word3 word1 word3 word4 word1 word5 word6"
+list1 = str1.split(' ')
+
+
+newDict = {}
+for word in list1:
+    if word in newDict:
+        newDict[word] +=1
+    else:
+        newDict[word] =1
+
+print(newDict)"""
+
+"""Define a class called Triangle, with 2 Attributes: Base and Height.
+Create a object with Name obj1 and ensure print(obj1) shows both Base and Height."""
+
+"""class Triangle:
+    
+    def __init__(self,base,height):
+        self.base = base
+        self.height = height
+    
+    def __str__(self):
+        return str(self.base) + str(self.height)
+    
+obj1 = Triangle(1,2)
+print(obj1)"""
+
+
+"""www.bank.com
+
+urls.py
+
+views.py
+
+def index(request):
+    model = Bank.objects.all()
+    contect = {
+        'model': model
+    }
+
+    return render(request,'index.html', context)"""
+
+
+
+# django signals'
+# sessions
+# decorators
+# exception handling
+# Projects
+
+
+
