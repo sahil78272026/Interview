fecbcc8caaa80a93030632e5c7690cb2f6ccd414
sahil.garg78272026@gmail.com <sahil.garg78272026@gmail.com>
2023-06-05 20:47:30 +0530

    zensar 2nd round

--- zensar.py
+++ zensar.py
@@ -5,4 +5,16 @@
 # how do you handle keyerror in json file
 # decorators, where used?
 # django arch
-#
+# have you created CI-CD , used jenkins?
+
+
+# Second Round: 05-June-2023
+
+# l1 = [[2,5,9],[10,13,19]]
+# make it in sinlge list = [2,5,9,10,13,19]
+
+# difference between sort and sorted
+# sort a dictionary
+# generators
+# how memory works in python
+# list comprehension
