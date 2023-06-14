f3210ec1913b50d3350abf2f99f6fd8eb2c27250
sahil.garg78272026@gmail.com <sahil.garg78272026@gmail.com>
2023-06-14 14:35:07 +0530

    staqu

--- staqu.py
+++ staqu.py
@@ -1,7 +1,8 @@
+# data:14-June-2023, HR- Sakshi: 7014129682
+
 # what tools you have used in networking
 # how to optimize database queries
 # mixins
 # have you used cython
 # caching
 # Celery
-# have you used multithreading and multiprocessing in your project
