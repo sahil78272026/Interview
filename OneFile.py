39d1250eb15bd82c437996dec62caa4e46cf4cc9
sahil.garg78272026@gmail.com <sahil.garg78272026@gmail.com>
2023-12-22 19:10:10 +0530

    gedu services

--- OneFile.py
+++ OneFile.py
@@ -236,4 +236,62 @@ Candidate.objects.using('student').all()
 # what is lazy loading
 # atomic query in django
 # generators
+# cookie riding
+# middleware
+# content type
+# abstract
+# fake migrations
+# throttling
+# status code
+# celery
+# inner joins, query format
+# SQL
+# JWT
+# Views in DRF
+
+# projects
+# multithreading
+# request response cycle
+# migrations
+# context processor
+
+
+# '''
+# convert a string to uppercase in python without using any built-in  function, you have to convert each character to its uppercase equivalent.
+# Input : Sahil Garg
+# Output : SAHIL GARG
+# '''
+# str1 = 'Sahil Garg'
+# # 65 to 90 upper case
+# # 97 to 122 lower case
+# # newStr = ''
+# # for ch in str1:
+# #     # print(ch)
+# #     ascii_value = ord(ch)
+# #     newchar_ascii = ascii_value+32
+# #     newStr+= chr(newchar_ascii)
+
+# # print(newStr)
+
+# lst = str1.split(' ')
+# print(lst)
+
+# newStr1 = ''.join(lst)
+# print(newStr1)
+
+# newStr2 = ''
+# for ch in newStr1:
+#     # print(ch)
+#     ascii_value = ord(ch)
+#     newchar_ascii = ascii_value - 32
+#     newStr2 += (chr(newchar_ascii))
+
+# print(newStr2)
+
+# star pattern
+# star = '*'
+# for i in range(11):
+#     print(star)
+
+
 
---
+++ gedu_services.py
@@ -0,0 +1,52 @@
+
+# projects
+# multithreading
+# request response cycle
+# migrations
+# context processor
+
+
+# '''
+# convert a string to uppercase in python without using any built-in  function, you have to convert each character to its uppercase equivalent.
+
+
+# Input : Sahil Garg
+# Output : SAHIL GARG
+# '''
+
+# str1 = 'Sahil Garg'
+
+# # 65 to 90 upper case
+# # 97 to 122 lower case
+
+# # newStr = ''
+# # for ch in str1:
+# #     # print(ch)
+# #     ascii_value = ord(ch)
+# #     newchar_ascii = ascii_value+32
+# #     newStr+= chr(newchar_ascii)
+
+# # print(newStr)
+
+# lst = str1.split(' ')
+# print(lst)
+
+# newStr1 = ''.join(lst)
+# print(newStr1)
+
+# newStr2 = ''
+# for ch in newStr1:
+#     # print(ch)
+#     ascii_value = ord(ch)
+#     newchar_ascii = ascii_value - 32
+#     newStr2 += (chr(newchar_ascii))
+
+# print(newStr2)
+
+# star pattern
+
+
+star = '*'
+for i in range(11):
+    print(star)
+
